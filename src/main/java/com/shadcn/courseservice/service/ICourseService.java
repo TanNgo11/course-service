b91dddca0f318dd13bfdf9aebf7419138628d39b
Bui Hoang Huy <huy.buihoang2301@gmail.com>
2025-05-22 17:20:38 +0700

    fix

--- src/main/java/com/shadcn/courseservice/service/ICourseService.java
+++ src/main/java/com/shadcn/courseservice/service/ICourseService.java
@@ -55,4 +55,6 @@ public interface ICourseService {
     PageResponse<CourseResponse> findAllCoursesBySemesterId(Long semesterId, int current, int pageSize);
 
     void updateCourseConstraint(Long id, UpdateConstraintCourseRequest request);
+
+
 }
